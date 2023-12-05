90e48a0d76df2e2d7b68d04d8c834370cdc41d6f
Rferencsik <robert.ferencsik@student.uni-miskolc.hu>
2023-12-05 12:44:30 +0100

    From windows
    Feladat 1 és egyéb dolgok

---
+++ SemTaskBQLOTW/2_Feladat/lakohely_gtfs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+zip_url="https://gtfsapi.mvkzrt.hu/gtfs/gtfs.zip"
+output_folder="/home/robert/Dokumentumok/BQLOTWSzgepArchGyak/SemTaskBQLOTW/ShellScript/GTFS"
+
+if [ ! -d "$output_folder" ]; then
+    mkdir "$output_folder"
+fi
+
+cd "$output_folder" || exit
+
+if [ -e "downloaded_file.zip" ]; then
+    echo "Deleting previous version of downloaded ZIP file."
+    rm downloaded_file.zip
+fi
+
+if [ "$(ls -A .)" ]; then
+    echo "Deleting previous content of the extraction folder."
+    rm -r *
+fi
+
+wget "$zip_url" -O downloaded_file.zip
+
+if [ $? -eq 0 ]; then
+    echo "ZIP file download successful."
+    unzip downloaded_file.zip
+
+    if [ $? -eq 0 ]; then
+        echo "Extraction successful."
+    else
+        echo "Extraction failed."
+        exit 1
+    fi
+else
+    echo "ZIP file download failed."
+    exit 1
+fi
+touch lakohelyutvonal.txt
+output_file="lakohelyutvonal.txt"
+
+grep "$Szent Anna tér" "/home/robert/Dokumentumok/BQLOTWSzgepArchGyak/SemTaskBQLOTW/ShellScript/GTFS/routes.txt" >>  "$output_file"
+# Ellenőrizzük, hogy a szűrés sikeres volt-e
+if [ $? -eq 0 ]; then
+    echo "A szűrt eredmények ki lettek írva a $output_file fájlba."
+else
+    echo "Hiba történt a szűrés során."
+fi
